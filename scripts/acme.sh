0403ce2f899597581d71c4898bc6e492db0cd084
Julien Bongars <julienbongars23@gmail.com>
2025-10-30 13:30:34 +0800

    feat: move to firefox instead of chrome

--- scripts/acme.sh
+++ scripts/acme.sh
@@ -3,7 +3,7 @@ set -eu
 
 focused_app=$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .app_id')
 
-if echo "$focused_app" | grep -q "chrome"; then
+if echo "$focused_app" | grep -q "firefox"; then
     case "$1" in
         "BTN_SIDE"|"button8")
             wtype -M ctrl -k w -m ctrl
